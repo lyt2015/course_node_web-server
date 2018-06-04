c6ed5ba0ad03f8080d203389f80dfb5b0a2c57bb
lyt2015 <lyt2015@users.nonreply.github.com>
2018-06-04 12:48:10 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+
+  let log = `${now}: ${req.method} - ${req.url}`;
+
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\r\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+
+  next();
+});
+/* app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+}); */
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1 style="color: red">Hello There!</h1>');
+  res.render('home.hbs', {
+    title: 'Website Alpha',
+    message: 'Welcome to My Awesome Website',
+    getYear: 3000
+  });
+});
+
+app.get('/ffv', (req, res) => {
+  res.render('ffv.hbs', {
+    title: 'Final Fantasy V',
+    message: 'Welcome to My Awesome Website',
+    getYear: 3000
+  });
+});
+
+app.get('/error', (req, res) => {
+  res.send({
+    errorMessage: 'Something went wrong, please understand.'
+  });
+});
+
+const port = 4999;
+app.listen(port, () => {
+  console.log(`Server is now live at port: ${port}.`);
+});
